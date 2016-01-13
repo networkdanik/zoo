ba948af07640707ad50b021fd0adb452a73b9660
networkdanik <networkdanik@gmail.com>
2016-01-13 20:02:02 +0100

    New Person Aleks

--- src/main/java/com/zoo/Executor.java
+++ src/main/java/com/zoo/Executor.java
@@ -6,12 +6,20 @@ package com.zoo;
 public class Executor {
     public static void main(String[] args) {
     Executor executor = new Executor();
-    Person pasha = new Person("Pawel;", "Kulakowski;", 35, (long) 72.5);
+    Person pasha = new Person("Pawel;","Kulakowski;", 35, (long) 72.5);
+        Person sasha = new Person("Aleks;","Gross ;", 38, (long) 78.5);
 
     executor.printPersonPasha(pasha);
-}
+        executor.printPersonSasha(sasha);
 
-        private  void printPersonPasha(Person person){
+
+}
+    private  void printPersonPasha(Person person) {
+        System.out.println("Person: " + "Firstname -" + " " + person.getFirstName()
+                + " " + "Lasname -" + " " + person.getLastName()
+                + " Age - " + person.getAge() + " " + " Weight " + person.getWeight());
+    }
+        private  void printPersonSasha(Person person){
             System.out.println("Person: " + "Firstname -" +" " + person.getFirstName()
                     + " " + "Lasname -" + " " + person.getLastName()
                     + " Age - " + person.getAge() + " " + " Weight " +person.getWeight());
