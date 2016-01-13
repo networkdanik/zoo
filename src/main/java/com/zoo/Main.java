e9e98a6f5e20091e24110627e82e06b867282bc4
Pavel Kulakovsky <pkulakovsky@gmail.com>
2016-01-13 17:10:16 +0200

    change class Main

--- src/main/java/com/zoo/Main.java
+++ src/main/java/com/zoo/Main.java
@@ -3,6 +3,11 @@ package com.zoo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Zoo");
+        Person person = new Person();
+        person.setName("Pavel");
+
+        String name = person.getName();
+
+        System.out.println("Person: " + name);
     }
 }
