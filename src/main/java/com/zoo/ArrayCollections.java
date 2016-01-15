3dc501e16105658afc19adabd798acfabb036461
networkdanik <networkdanik@gmail.com>
2016-01-15 17:58:31 +0100

    Examples ArrayCollections

--- src/main/java/com/zoo/ArrayCollections.java
+++ src/main/java/com/zoo/ArrayCollections.java
@@ -1,10 +1,13 @@
 package com.zoo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by networkdanik on 1/15/16.
  */
 public class ArrayCollections {
-
+/*
         final  static String[] WEEKDAY ={"MON", "WEM", "TUE", "THU", "FRI", "SAT", "SUN"};
 
         public static void main(String[] args) {
@@ -13,7 +16,17 @@ public class ArrayCollections {
                 System.out.println(day + " " + "is number under" + " " + i);
                 i++;
 
-            }
+ */
+public static void main(String[] args) {
+
+    List<String> strList = new ArrayList<>();
+         strList.add("John");
+         strList.add("Willi");
+         strList.add("Iwan");
+
+    for (String str : strList){
+        System.out.println(str);
+    }
         }
     }
 
