f2873c89e31a77bfff7841abe0500bc6295b480b
networkdanik <networkdanik@gmail.com>
2016-01-15 12:40:07 +0100

    Examples Array

---
+++ src/main/java/com/zoo/ExamplesArrayList.java
@@ -0,0 +1,17 @@
+package com.zoo;
+
+/**
+ * Created by networkdanik on 1/15/16.
+ */
+public class ExamplesArrayList {
+   final  static String[] WEEKDAY ={"MON", "WEM", "TUE", "THU", "FRI", "SAT", "SUN"};
+
+    public static void main(String[] args) {
+        int i = 0;
+        for (String day : WEEKDAY){
+            System.out.println(day + " " + "is number under" + " " + i);
+            i++;
+
+        }
+    }
+}
