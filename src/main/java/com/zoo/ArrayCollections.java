4cbd7917db1d09413a5309a2932ed6ff9bde069b
networkdanik <networkdanik@gmail.com>
2016-01-15 20:51:35 +0100

    Examples ArrayCollections

--- src/main/java/com/zoo/ArrayCollections.java
+++ src/main/java/com/zoo/ArrayCollections.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * Created by networkdanik on 1/15/16.
  */
-public class ArrayCollections {
+public class ArrayCollections  {
 /*
         final  static String[] WEEKDAY ={"MON", "WEM", "TUE", "THU", "FRI", "SAT", "SUN"};
 
@@ -19,15 +19,34 @@ public class ArrayCollections {
  */
 public static void main(String[] args) {
 
-    List<String> strList = new ArrayList<String>();
+    /*List<String> strList = new ArrayList<String>();
          strList.add("John");
          strList.add("Willi");
          strList.add("Iwan");
+         strList.add(3,"test");
 
     for (String str : strList){
         System.out.println(str);
+        */
+    Animal dog = new Dog();
+    Animal cat = new Cat();
+
+
+    List<Animal> animals = new ArrayList<Animal>();
+
+    animals.add(dog);
+    animals.add(cat);
+
+    for (int i = 0; i <5;  i++){
+        animals.add(new Dog());
     }
-        }
+    for (Animal animal : animals){
+        animal.voice();
+    }
+
+    }
+
+
     }
 
 
